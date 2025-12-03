ab7e8bc2c5f52a979a3ef6eb5af1c49b633840a7
jaden-jov <jaden.jov@gmail.com>
2025-12-02 20:06:47 -0600

    initial commit

---
+++ website/src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+  title: `placeholder`,
+  description: `placeholder`,
+  url: `https://aimdutd.org`, // No trailing slash!
+  name: `AIMD`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: false,
+  showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
+};
